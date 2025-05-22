d9fa5c002aa97159e5742eb5c1d1349b3a441506
Tymofii Katyshev <tymakat@gmail.com>
2025-05-22 20:44:17 +0200

    feat: added first endpoint to retreive top artists
    - 100% of initial backend for getting top 500 singers is now fully implemented
    - Added new endpoint "get_top_artists"
    - Execution time - 40 ms.
    - Everything works as it should.

--- backend/main.py
+++ backend/main.py
@@ -1,22 +1,27 @@
 from apscheduler.schedulers.background import BackgroundScheduler
 from apscheduler.triggers.cron import CronTrigger
+from apscheduler.schedulers.asyncio import AsyncIOScheduler
 import pytz
 from webscraping import get_info_on_top_singers
 import redis
 from datetime import datetime
+from fastapi import FastAPI
+import time
 
 REDIS_HOST = "localhost"
 REDIS_PORT = 6379
 
 r = redis.Redis(host=REDIS_HOST, port=REDIS_PORT, decode_responses=True)
 
-scheduler = BackgroundScheduler()
+app = FastAPI()
+
+scheduler = AsyncIOScheduler()
 ARTIST_UPDATE_FREQUENCY_PER_DAY = 4
+scheduler.start()
 
 @scheduler.scheduled_job(
         id="update_artist_leaderboard",
         trigger=CronTrigger(hour='*', minute='*', second='*/10', timezone=pytz.UTC, jitter=0))
-
 def update_artist_leaderboard():
     #average execution time - 1.6 - 2 seconds
     info_on_top_singers = get_info_on_top_singers()
@@ -27,7 +32,17 @@ def update_artist_leaderboard():
     #measure the execution time
     # print(info_on_top_singers[1])
 
+@app.get("/get_top_artists")
+def get_top_artists():
+    exec_start = time.time()
+    last_updated_at = r.get("top_listened_artists:last_updated_at")
+    info_on_top_singers = r.json().get("top_listened_artists:content", "$")
+    exec_end = time.time()
+    print(exec_end - exec_start)
+    return {"Last updated": last_updated_at, "Top 500": info_on_top_singers}
+
 #just a temporary solution till we setup the fastapi server
+"""
 if __name__ == "__main__":
     scheduler.start()
     try:
@@ -35,3 +50,4 @@ if __name__ == "__main__":
         import time;  time.sleep(1e9)
     except (KeyboardInterrupt, SystemExit):
         scheduler.shutdown()
+"""
