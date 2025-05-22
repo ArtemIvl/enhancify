8ffa5a680284fec8f0aea76604f5092e39bfbfcc
Tymofii Katyshev <tymakat@gmail.com>
2025-05-22 16:57:38 +0200

    feat: implemented webscraping component

---
+++ backend/webscraping.py
@@ -0,0 +1,33 @@
+import requests, re, textwrap, difflib
+from bs4 import BeautifulSoup
+import json
+
+#make an initial webscraping request to a website
+URL = "https://chartmasters.org/most-monthly-listeners-on-spotify/"
+
+#endpoint that website uses to get the information about artists
+AJAX  = ("https://chartmasters.org/wp-admin/admin-ajax.php"
+         "?action=get_wdtable&table_id=74")
+
+#request headers (for cleanliness)
+headers = {"User-Agent": "Mozilla/5.0", "Accept": "text/html"}
+
+resp    = requests.get(URL, headers=headers, timeout=15)
+html    = resp.text
+
+#find nonce using beautifulsoup webscraper
+#nonce is like a safety keyword, generated once every 12 hours
+nonce = (BeautifulSoup(html, "lxml")
+         .find("input", id="wdtNonceFrontendServerSide_74")["value"])
+            
+payload = {
+    "draw":"1", "start":"0", "length":"-1",
+    "order[0][column]":"4", "order[0][dir]":"desc",
+    "wdtNonce": nonce,
+    "sRangeSeparator":"|"
+}
+
+data = requests.post(AJAX, data=payload,
+                     timeout=15).json()["data"]
+
+print(json.dumps(data, indent=2))   # first 3 rows
