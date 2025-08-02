8d284c45dec763aade0bc4378d5e068db3a97ad7
Tymofii Katyshev <tymakat@gmail.com>
2025-08-02 17:07:19 +0400

    feat: MyArtists tab fully enhanced

--- frontend/src/components/SpotifyArtistCard.jsx
+++ frontend/src/components/SpotifyArtistCard.jsx
@@ -2,11 +2,48 @@ import { FaSpotify } from "react-icons/fa";
 
 export default function SpotifyArtistCard({ artist, rank }) {
   const imageUrl = artist.images?.[0]?.url;
-  const genres =artist.genres.join(", ");
   const followers = artist.followers.total;
   const externalUrl = artist.external_urls?.spotify;
-  const popularity = artist.popularity;
-
+  function getPopularityLabel() {
+    if (artist.popularity >= 90) { //HIT
+      return "Legend"
+    }
+    else if (artist.popularity < 90 && artist.popularity >= 75) { //VERY POPULAR
+      return "Superstar"
+    }
+    else if (artist.popularity < 75 && artist.popularity >= 60) { //POPULAR
+      return "Popular"
+    }
+    else if (artist.popularity < 60 && artist.popularity >= 40) { //Average
+      return "Known"
+    }
+    else if (artist.popularity >= 20 && artist.popularity < 40) { //Niche
+      return "Niché"
+    }
+    else {
+      return "Undiscovered"
+    }
+  }
+  function getPopularityIcon() {
+    if (artist.popularity >= 90) { //HIT
+      return "local_fire_department"
+    }
+    else if (artist.popularity < 90 && artist.popularity >= 75) { //VERY POPULAR
+      return "hotel_class"
+    }
+    else if (artist.popularity < 75 && artist.popularity >= 60) { //POPULAR
+      return "star"
+    }
+    else if (artist.popularity < 60 && artist.popularity >= 40) { //Average
+      return "star_half"
+    }
+    else if (artist.popularity >= 20 && artist.popularity < 40) { //Niche
+      return "diamond"
+    }
+    else {
+      return "tour"
+    }
+  }
   const bgGradient =
     rank === 1 ? "linear-gradient(to bottom, #FFDF00, #E9D293)" :
     rank === 2 ? "linear-gradient(to bottom, #BEC0C2, #EEF2F3)" :
@@ -14,40 +51,18 @@ export default function SpotifyArtistCard({ artist, rank }) {
     "#E5E4E2";
 
   return (
-    <div className="w-full grid grid-cols-[26%_74%] items-center bg-[#E5E4E2] rounded-xl pl-4 py-4">
+    <div className="w-full items-center bg-[#E5E4E2] rounded-2xl pl-4 py-5">
       <div className="flex items-center gap-8">
         <div className="text-lg font-semibold w-8 text-center rounded-md py-1"
         style={{ background: bgGradient }}>{rank}</div>
         <div className="w-14 min-w-14 h-14 rounded-md overflow-hidden">
           <img src={imageUrl} alt={artist.name} className="w-full h-full object-cover" />
         </div> 
-        <div className="font-semibold truncate">{artist.name}</div>
-      </div>
-      
-      <div className="grid grid-cols-4 items-center">
-        <div className="text-sm text-center">{artist.genres.length ? genres : "N/A"}</div>
-        <div className="text-sm text-center">{popularity}/100</div>
-        <div className="text-sm text-center">
-          {followers >= 1_000_000
-            ? `${(followers / 1_000_000).toFixed(1)}M`
+        <div className="truncate mt-[0.5vh]"><div className="font-semibold">{artist.name}</div><div className="flex text-white text-[13px] mt-[1.2vh] gap-3 "><div className="px-2 py-1 rounded-xl bg-[#2e2e2e]">{artist.genres.length ? artist.genres[0].charAt(0).toUpperCase() + artist.genres[0].slice(1) : (followers >= 1_000_000
+            ? `${(followers / 1_000_000).toFixed(1)}M fans`
             : followers >= 1_000
-            ? `${(followers / 1_000).toFixed(1)}K`
-            : followers.toString()}
-        </div>
-
-        <div className="flex justify-center">
-          <a
-            href={externalUrl}
-            target="_blank"
-            rel="noopener noreferrer"
-            className="group relative flex items-center w-30 h-12 bg-black rounded-md px-4 overflow-hidden"
-          >
-            <span className="absolute ml-8 text-white text-sm opacity-0 group-hover:opacity-100 group-hover:translate-x-0 translate-x-4 transition-all duration-500 z-0 whitespace-nowrap">
-              Spotify
-            </span>
-            <FaSpotify className="relative text-green-500 text-2xl mx-auto transform transition-all duration-500 group-hover:-translate-x-8 group-hover:-rotate-[360deg]" />
-          </a>
-        </div>
+            ? `${(followers / 1_000).toFixed(1)}K fans`
+            : followers.toString())}</div><div className="px-2 py-1 rounded-xl bg-[#2e2e2e]"><span className="material-icons-outlined icons-tweaked">{getPopularityIcon()}</span>{getPopularityLabel()}</div></div></div>
       </div>
     </div>
   );
--- frontend/src/pages/MyArtists.jsx
+++ frontend/src/pages/MyArtists.jsx
@@ -19,7 +19,7 @@ export default function TopContent() {
   const [search, setSearch] = useState("");
   const [openDropdown, setOpenDropdown] = useState(null);
   const [unauthorized, setUnauthorized] = useState(false);
-
+  const [descAscFilter, setDescAscFilter] = useState("desc");
   useEffect(() => {
     if (!token) {
       setIsLoading(false);
@@ -69,9 +69,9 @@ export default function TopContent() {
       <div className="text-2xl font-semibold font-bold py-3">Your most listened artists and tracks</div>
       <div className="mb-[2vw] italic">No need to wait for Wrapped.</div>
       <div className="flex w-full justify-between items-center gap-4">
-        <div className="flex gap-4 w-1/3">
+        <div className="flex gap-4 w-[25%]">
           <button
-            className={`rounded-md py-3 px-6 flex cursor-pointer font-light text-sm items-center gap-3 ${tab === "tracks" ? "bg-[#2e2e2e] text-white" : "bg-[#d3cfce]"}`}
+            className={`rounded-xl py-3 px-7 flex cursor-pointer font-base text-sm items-center gap-3 ${tab === "tracks" ? "bg-[#2e2e2e] text-white" : "bg-[#d3cfce]"}`}
             onClick={() => setTab("tracks")}
             onMouseDown={e => (e.currentTarget.style.transform = "scale(0.95)")}
             onMouseUp={e => (e.currentTarget.style.transform = "scale(1)")}
@@ -81,7 +81,7 @@ export default function TopContent() {
             Top Songs
           </button>
           <button
-            className={`px-6 py-3 rounded-md flex text-sm cursor-pointer font-light items-center gap-3 ${tab === "artists" ? "bg-[#2e2e2e] text-white" : "bg-[#d3cfce]"}`}
+            className={`px-7 py-3 rounded-xl flex text-sm cursor-pointer font-base items-center gap-3 ${tab === "artists" ? "bg-[#2e2e2e] text-white" : "bg-[#d3cfce]"}`}
             onClick={() => setTab("artists")}
             onMouseDown={e => (e.currentTarget.style.transform = "scale(0.95)")}
             onMouseUp={e => (e.currentTarget.style.transform = "scale(1)")}
@@ -90,23 +90,51 @@ export default function TopContent() {
             <FaMusic className="w-4 h-4"/>
             Top Artists
           </button>
-        </div>
+          </div>
+          <div className="h-[2.5vw] w-[2%]"><div className="w-[2px] h-full color-[#2e2e2e] bg-[#2e2e2e]"></div></div>
 
-        <div className="flex gap-8 w-2/3">
-          <div className="flex items-center w-3/4 relative rounded-2xl shadow-md">
+        <div className="flex gap-8 w-[67%]">
+          <div className="flex h-[2.75vw] text-[14px]">
+          <button onClick={() => setTimeRange("short_term")} className={`cursor-pointer rounded-tl-3xl rounded-bl-3xl w-[9vw] py-3 ${
+            timeRange === "short_term"
+              ? "bg-[#f5f5f5] text-black border"
+              : "bg-[#2e2e2e] text-white"
+          }`}>Last 4 weeks</button>
+          <div className=" h-[100%] bg-white w-[1px]"></div>
+          <button onClick={()=>setTimeRange("medium_term")} className={`cursor-pointer w-[9vw] py-3 ${
+            timeRange === "medium_term"
+              ? "bg-[#f5f5f5] text-black border"
+              : "bg-[#2e2e2e] text-white"
+          }`}>Last 6 months</button>
+          <div className="h-[100%] bg-white w-[1px]"></div>
+          <button onClick={()=>setTimeRange("long_term")} className={`w-[9vw] rounded-br-3xl rounded-tr-3xl py-3 cursor-pointer ${
+            timeRange === "long_term"
+              ? "bg-[#f5f5f5] text-black border"
+              : "bg-[#2e2e2e] text-white"
+          }`}>All time</button>
+          </div>
+          <div className="ml-[2vw] flex items-center w-[35%] relative rounded-3xl shadow-md">
             <input
               type="text"
               placeholder={tab === "artists" ? "Search artists by name..." : "Search tracks by name..."}
               value={search}
               onChange={(e) => setSearch(e.target.value)}
-              className="w-full px-4 py-3 text-sm rounded-2xl bg-white text-black placeholder-[#868686] focus:outline-none"
+              className="w-full px-4 py-3 text-sm h-[2.6vw] rounded-2xl bg-white text-black placeholder-[#868686] focus:outline-none"
             />
+            
             <button
-            className="absolute right-0 top-0 h-full w-16 bg-black rounded-r-2xl flex items-center justify-center cursor-pointer">
-              <FaSearch className="text-white" />
+            className="absolute right-0 top-[4px] w-[18%] h-[2.3vw] w-16 text-black bg-white border-l rounded-r-2xl flex items-center justify-center ">
+              <FaSearch className="text-black" />
             </button>
           </div>
-
+          {/*
+          <div className="flex h-[2.75vw] text-[14px] cursor-pointer ml-[4vw]">
+          <button onClick={() => setDescAscFilter("desc")} className={`cursor-pointer rounded-tl-3xl rounded-bl-3xl w-[9vw] py-3 ${descAscFilter === "desc" ? "bg-[#f5f5f5] text-black border" : "bg-[#2e2e2e] text-white"}`}><span className="material-icons-outlined icons-tweaked">arrow_downward</span>Descending</button>
+          <div className=" h-[100%] bg-white w-[1px]"></div>
+          <button onClick={() => setDescAscFilter("asc")} className={`cursor-pointer rounded-br-3xl rounded-tr-3xl w-[9vw] py-3 ${descAscFilter === "asc" ? "bg-[#f5f5f5] text-black border" : "bg-[#2e2e2e] text-white"}`}><span className="material-icons-outlined icons-tweaked">arrow_upward</span>Ascending</button>
+          </div>
+          */}
+          {/*
           <DropdownComponent
             title="Select time range"
             options={["short_term", "medium_term", "long_term"]}
@@ -115,7 +143,7 @@ export default function TopContent() {
             isOpen={openDropdown === "time-range"}
             setOpenDropdown={setOpenDropdown}
             id="time-range"
-          />
+          /> */}
         </div>
       </div>
       {(!token || unauthorized) ? <Unauthorized></Unauthorized> : (<>
@@ -125,7 +153,7 @@ export default function TopContent() {
 
       {!isLoading && tab === "tracks" && (
         <div>
-          <div className="w-full sticky top-0 z-10 grid grid-cols-[26%_74%] py-4 text-[13px] bg-[#d3cfce] my-2 pl-4">
+          <div className="w-full sticky top-0 z-10 grid grid-cols-[26%_74%] mt-[2.5vh] py-4 text-[13px] bg-[#d3cfce] my-2 pl-4">
             {/* Left Column Header */}
             <div className="ml-[0.5vw] flex items-center gap-4">
               <div className="w-6 pl-4">#</div>
@@ -152,29 +180,11 @@ export default function TopContent() {
       )}
 
       {!isLoading && tab === "artists" && (
-        <div>
-          <div className="w-full sticky top-0 z-10 grid grid-cols-[26%_74%] py-4 text-[12px] bg-[#d3cfce] my-2 pl-4">
-            {/* Left Column Header */}
-            <div className="flex items-center gap-4">
-              <div className="w-6 pl-4">#</div>
-              <div className="w-24" /> {/* Empty space for image */}
-              <div>Name</div>
-            </div>
-
-            {/* Right Column Headers */}
-            <div className="grid grid-cols-4 items-center text-center">
-              <div>Genres</div>
-              <div>Popularity Index</div>
-              <div>Followers on Spotify</div>
-              <div>Spotify Profile link</div>
-            </div>
-          </div>
-          <div className="flex flex-col gap-4 w-full">
+          <div className="flex flex-col gap-x-6 gap-y-5 w-full grid grid-cols grid-cols-[repeat(auto-fill,_minmax(34ch,1fr))] mt-[6.5vh]">
             {filteredArtists.map((artist, index) => (
               <SpotifyArtistCard key={artist.id} artist={artist} rank={artist.rank} />
             ))}
           </div>
-        </div>
       )}
       <div className="mb-2"></div></>)}
     </div>
